e2a17859c5b855c57e2eacd9130ed10ca5c6e2c1
Sean L <nolinyc@gmail.com>
2020-04-14 17:43:08 -0700

    Add VS1053 MP3 driver for SJ2

---
+++ sjtwo-c/projects/lpc40xx_freertos/l3_drivers/mp3_vs1053.h
@@ -0,0 +1,67 @@
+/*********************************************************
+ * This is a driver for VS1053 Codec board with amplifier
+ * Designed for SJSU SJ2 dev board
+ *
+ * Written by Yucheng Lin(Sean), Bo An, and Alex Chavez
+ *
+ *********************************************************/
+#pragma once
+
+#include "gpio.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+static gpio_s RST, DREQ, XDCS, MP3CS;
+typedef char mp3_song_name_t[32];
+typedef uint8_t mp3_data_block_t[512];
+
+#define VS1053_SCI_READ 0x03
+#define VS1053_SCI_WRITE 0x02
+
+#define VS1053_REG_MODE 0x00
+#define VS1053_REG_STATUS 0x01
+#define VS1053_REG_BASS 0x02
+#define VS1053_REG_CLOCKF 0x03
+#define VS1053_REG_DECODETIME 0x04
+#define VS1053_REG_AUDATA 0x05
+#define VS1053_REG_WRAM 0x06
+#define VS1053_REG_WRAMADDR 0x07
+#define VS1053_REG_HDAT0 0x08
+#define VS1053_REG_HDAT1 0x09
+#define VS1053_REG_VOLUME 0x0B
+
+#define VS1053_GPIO_DDR 0xC017
+#define VS1053_GPIO_IDATA 0xC018
+#define VS1053_GPIO_ODATA 0xC019
+
+#define VS1053_INT_ENABLE 0xC01A
+
+#define VS1053_MODE_SM_DIFF 0x0001
+#define VS1053_MODE_SM_LAYER12 0x0002
+#define VS1053_MODE_SM_RESET 0x0004
+#define VS1053_MODE_SM_CANCEL 0x0008
+#define VS1053_MODE_SM_EARSPKLO 0x0010
+#define VS1053_MODE_SM_TESTS 0x0020
+#define VS1053_MODE_SM_STREAM 0x0040
+#define VS1053_MODE_SM_SDINEW 0x0800
+#define VS1053_MODE_SM_ADPCM 0x1000
+#define VS1053_MODE_SM_LINE1 0x4000
+#define VS1053_MODE_SM_CLKRANGE 0x8000
+
+#define VS1053_SCI_AIADDR 0x0A
+#define VS1053_SCI_AICTRL0 0x0C
+#define VS1053_SCI_AICTRL1 0x0D
+#define VS1053_SCI_AICTRL2 0x0E
+#define VS1053_SCI_AICTRL3 0x0F
+
+#define VS1053_DATABUFFERLEN 32
+
+bool mp3__init();
+bool data_request();
+
+void mp3__sci_write(uint8_t address, uint16_t data);
+uint16_t mp3__sci_read(uint8_t address);
+
+void mp3__sine_test(uint8_t n, uint16_t time_ms);
+void mp3__set_volume(uint8_t left, uint8_t right);
+void mp3__reset();
---
+++ sjtwo-c/projects/lpc40xx_freertos/l3_drivers/sources/mp3_vs1053.c
@@ -0,0 +1,159 @@
+/*********************************************************
+ * This is a driver for VS1053 Codec board with amplifier
+ * Designed for SJSU SJ2 dev board
+ *
+ * Written by Yucheng Lin(Sean), Bo An, and Alex Chavez
+ *
+ *********************************************************/
+
+#include "mp3_vs1053.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "ff.h"
+#include "gpio.h"
+#include "gpio_lab.h"
+#include "ssp2.h"
+
+static gpio_s RST, DREQ, XDCS, MP3CS;
+
+bool data_request() { return gpio__get(DREQ); }
+
+void mp3__sci_write(uint8_t address, uint16_t data) {
+  gpio__reset(MP3CS);
+  // while (!data_request()) {
+  //   vTaskDelay(5);
+  // }
+  ssp2__exchange_byte(VS1053_SCI_WRITE);
+  ssp2__exchange_byte(address);
+  ssp2__exchange_byte(data >> 8);
+  ssp2__exchange_byte(data & 0xFF);
+  gpio__set(MP3CS);
+  // vTaskDelay(5);
+}
+
+uint16_t mp3__sci_read(uint8_t address) {
+  // printf("sci read\n");
+  uint16_t data;
+  gpio__reset(MP3CS);
+  ssp2__exchange_byte(VS1053_SCI_READ);
+  ssp2__exchange_byte(address);
+  // delay__us(10);
+  // printf("sci read send address\n");
+
+  data = ssp2__exchange_byte(0x00);
+  data <<= 8;
+  data |= ssp2__exchange_byte(0x00);
+
+  // printf("sci read data\n");
+  gpio__set(MP3CS);
+
+  // printf("sci read finished\n");
+  return data;
+}
+
+bool mp3__init() {
+
+  printf("Enter mp3 init\n");
+  XDCS = gpio__construct_with_function(2, 1, GPIO__FUNCTION0);
+  gpio__set_as_output(XDCS);
+
+  DREQ = gpio__construct_with_function(2, 4, GPIO__FUNCTION0);
+  gpio__set_as_input(DREQ);
+
+  MP3CS = gpio__construct_with_function(2, 6, GPIO__FUNCTION0);
+  gpio__set_as_output(MP3CS);
+
+  RST = gpio__construct_with_function(1, 29, GPIO__FUNCTION0);
+  gpio__set_as_output(RST);
+
+  gpio__reset(RST);
+
+  gpio__set(MP3CS);
+  gpio__set(XDCS);
+
+  printf("Pin init finished\n");
+
+  ssp2__initialize(24);
+
+  printf("SSP2 init finished\n");
+
+  // mp3__sci_write(VS1053_REG_CLOCKF, 0x2000);
+  // mp3__sci_write(VS1053_REG_VOLUME, 0x3838);
+
+  mp3__reset();
+  // printf("mp3 reset finished\n");
+
+  printf("sci_read value: %u\n", mp3__sci_read(VS1053_REG_STATUS));
+
+  return (mp3__sci_read(VS1053_REG_STATUS) >> 4) & 0x0F;
+}
+
+void mp3__sine_test(uint8_t n, uint16_t time_ms) {
+  mp3__reset();
+
+  // uint16_t mode = mp3__sci_read(VS1053_REG_STATUS);
+  // mode |= 0x0020;
+
+  // mp3__sci_write(VS1053_REG_MODE, mode);
+
+  while (!data_request()) {
+    // delay__ms(5);
+  }
+  mp3__sci_write(VS1053_REG_MODE, 0x0820);
+
+  gpio__reset(XDCS);
+
+  ssp2__exchange_byte(0x53);
+  ssp2__exchange_byte(0xEF);
+  ssp2__exchange_byte(0x6E);
+  ssp2__exchange_byte(n);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+  gpio__set(XDCS);
+
+  delay__ms(1000);
+
+  gpio__reset(XDCS);
+
+  ssp2__exchange_byte(0x45);
+  ssp2__exchange_byte(0x78);
+  ssp2__exchange_byte(0x69);
+  ssp2__exchange_byte(0x74);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+  ssp2__exchange_byte(0x00);
+
+  gpio__set(XDCS);
+  printf("Finished sine test\n");
+}
+
+void mp3__set_volume(uint8_t left, uint8_t right) {
+  uint16_t volume;
+  volume = left;
+  volume <<= 8;
+  volume |= right;
+  mp3__sci_write(VS1053_REG_VOLUME, volume);
+}
+
+void mp3__reset() {
+  gpio__reset(RST);
+  delay__ms(100);
+  gpio__set(RST);
+
+  gpio__set(MP3CS);
+  gpio__set(XDCS);
+
+  delay__ms(100);
+
+  mp3__sci_write(VS1053_REG_MODE, VS1053_MODE_SM_SDINEW | VS1053_MODE_SM_RESET);
+  delay__ms(100);
+  delay__ms(100);
+
+  mp3__sci_write(VS1053_REG_CLOCKF, 0x2000);
+  mp3__set_volume(40, 40);
+}
