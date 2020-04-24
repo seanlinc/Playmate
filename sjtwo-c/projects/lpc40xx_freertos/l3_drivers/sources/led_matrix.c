226cefee2e567db51587ad47f63cba37246613aa
Sean Lin <47756378+seanlinc@users.noreply.github.com>
2020-04-23 17:31:14 -0700

    Write basic structure of LED Matrix  (#19)
    * Write basic structure of LED Matrix
    
    * Revert "Write basic structure of LED Matrix"
    
    This reverts commit d6587ebdffccb8c9c89fc6694d3f5168965821f2.
    
    * Write basic structure of LED matrix

---
+++ sjtwo-c/projects/lpc40xx_freertos/l3_drivers/sources/led_matrix.c
@@ -0,0 +1,62 @@
+/*********************************************************
+ * This is a driver for VS1053 Codec board with amplifier
+ * Designed for SJSU SJ2 dev board
+ *
+ * Written by Yucheng Lin(Sean), Bo An, and Alex Chavez
+ *
+ *********************************************************/
+
+#include "led_matrix.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "time.h"
+#include "gpio.h"
+#include "delay.h"
+
+void led__init()
+{
+    pin = gpio__construct_as_output(2, 0);
+    gpio__reset(pin); 
+}
+
+void led__show()
+{
+  uint8_t  *ptr, *end, p, bitMask;
+  ptr     =  pixels;
+  end     =  ptr + number_of_bytes;
+  p       = *ptr++;
+  bitMask =  0x80;
+
+  for(;;)
+  {
+      if(p & bitMask) {
+        // data ONE high
+        // min: 550 typ: 700 max: 5,500
+        gpio__set(pin);
+        delay__ns(550);
+        // min: 450 typ: 600 max: 5,000
+        gpio__reset(pin);
+        delay__ns(450);
+      } else {
+        // data ZERO high
+        // min: 200  typ: 350 max: 500
+        gpio__set(pin);
+        delay__ns(200);
+        // data low
+        // min: 450 typ: 600 max: 5,000
+        gpio__reset(pin);
+        delay__ns(450);
+      }
+      if(bitMask >>= 1) {
+        // Move on to the next pixel
+        asm("nop;");
+      } else {
+        if(ptr >= end) break;
+        p       = *ptr++;
+        bitMask = 0x80;
+      }
+  }
+}
